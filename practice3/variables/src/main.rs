51425365f5fe517b46337cb2e3d0574066a76d1c
Patre Maroti Suryakant <107026627+marotipatre@users.noreply.github.com>
2023-08-23 12:37:25 +0530

    incremental variables

---
+++ practice3/variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    let c=15;
+
+
+}
---
+++ practice3/variables/target/debug/deps/variables-8ac583aedb769df9.d
@@ -0,0 +1,5 @@
+f:\Rust\my_rust_programs\practice3\variables\target\debug\deps\variables-8ac583aedb769df9.rmeta: src\main.rs
+
+f:\Rust\my_rust_programs\practice3\variables\target\debug\deps\variables-8ac583aedb769df9.d: src\main.rs
+
+src\main.rs:
