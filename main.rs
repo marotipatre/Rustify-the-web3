f98610184df960dcf391d5fe9f0485a4ae7e96fa
Patre Maroti Suryakant <107026627+marotipatre@users.noreply.github.com>
2023-08-23 12:46:20 +0530

    pracitce 4

---
+++ main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+    let (x,y)=(450,32.68);
+    println!("The first number is {} and second number is {}",x,y);
+    let f=1_000_000;
+    println!("{}",f);
+    let x=255;
+    println!("The value of x in hexadecimal is {:x} and in octal is {:o} and in binary {:b}",x,x,x);
+    let d=255;
+    let v=35.6;
+    let s =d as f64+v;
+    println!("{}",s);
+    let mut h=5;
+    let h=5*5;
+    let h=20;
+    println!("{}",h);
+
+
+}
