5350ad75eb430227e7a905f4beb1a02e565a3acc
Patre Maroti Suryakant <107026627+marotipatre@users.noreply.github.com>
2023-08-23 12:34:35 +0530

    variables

---
+++ practice2/variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    let x=15;
+    
+
+}
---
+++ practice2/variables/target/debug/deps/variables-61fc9784e4eaa579.d
@@ -0,0 +1,5 @@
+f:\Rust\my_rust_programs\practice2\variables\target\debug\deps\variables-61fc9784e4eaa579.rmeta: src\main.rs
+
+f:\Rust\my_rust_programs\practice2\variables\target\debug\deps\variables-61fc9784e4eaa579.d: src\main.rs
+
+src\main.rs:
